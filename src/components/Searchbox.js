29011ffb71c6519c7681153841028ca2d0aad2cc
Otabek Ulashov <otabek83019@gmail.com>
2021-01-22 23:46:54 +0500

    Robofriends

---
+++ src/components/Searchbox.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Searchbox = ({searchField,SearchChange}) => {
+    return(
+        <div className="pa2">
+            <input 
+                className="pa3 ba b--green bg-lightest-blue"
+                type="search"
+                placeholder="Search robots"
+                onChange={SearchChange}
+             />
+        </div>
+        
+    )
+}
+
+export default Searchbox;
---
+++ src/container/App.js
@@ -0,0 +1,51 @@
+import React, { Component }from 'react'
+import {robots} from '../robots.js';
+import CardList from '../components/CardList';
+import SearchBox from '../components/Searchbox';
+import '../container/App.css';
+import Scroll from '../components/Scroll';
+import ErrorBoundle from '../components/ErrorBoundle';
+class App extends Component {
+  
+    constructor() {
+        super()
+        this.state={
+            'robots':[],
+            'searchfields':''
+        }
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users =>this.setState({robots:users}))
+        
+    }
+
+    onSearchChange=(event)=>{
+        this.setState({ searchfields: event.target.value});
+    }
+    render(){
+        const {robots,searchfields} =this.state;
+        const filteredRobots =robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfields.toLowerCase())
+        });
+        return !robots.length ?
+         <h1 className="tc">Loading</h1>:
+          (
+                <div className="tc">
+                    <h1 className="f1">Robofriends</h1>
+                    <SearchBox SearchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <ErrorBoundle>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundle>
+                    </Scroll> 
+                    
+                </div>  
+            )
+        
+        
+    }
+    
+}
+export default App;
